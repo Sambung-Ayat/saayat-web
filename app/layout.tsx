f0dd4732b79a3fb132b50c1f6e90ee42d072c6c1
dayeeen <nubicodes@gmail.com>
2026-06-18 10:01:17 +0700

    feat: add Google Ads script to the head of the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,6 +39,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5098317968908735"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${amiri.variable} antialiased relative`}
         suppressHydrationWarning
